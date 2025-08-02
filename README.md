0502827c4965cca746b957bfab46272672cd6b70
Abhishek Awana <abhishekawana@Abhisheks-MacBook-Air.local>
2025-08-02 20:34:17 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# didactic-dollop
-spirityfam
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="w-full py-8 px-4 bg-dark text-white flex flex-col items-center mt-8">
-    <p className="text-gray-100 text-sm">&copy; {new Date().getFullYear()} Spirity Wellness Club. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer; 
